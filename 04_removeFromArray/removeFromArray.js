1c0ecbbc51cfdee9df1211031985fd18029fb397
aj-cabreros <174784983+aj-cabreros@users.noreply.github.com>
2024-12-17 15:26:26 -1000

    [+] Complete Exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (array, ...deletions) {
+  for (let n = 0; n < deletions.length; n++) {
+    do {
+      index = array.indexOf(deletions[n])
+      if (index >= 0) array.splice(index, 1)
+    } while (index >= 0)
+  }
+  return array
+}
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
