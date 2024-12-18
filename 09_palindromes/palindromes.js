2911330032a3a7c0f52bce3b2c02dc5f71e846b8
aj-cabreros <174784983+aj-cabreros@users.noreply.github.com>
2024-12-18 13:03:59 -1000

    [+] Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const arr = str.replace(/\W/g, '').toLowerCase().split('')
+  let front = 0
+  let back = arr.length - 1
+
+  while (front <= back) {
+    if (arr[front] !== arr[back]) break
+    front++
+    back--
+  }
+
+  if (front >= back) return true
+  else return false
 
 };
 
