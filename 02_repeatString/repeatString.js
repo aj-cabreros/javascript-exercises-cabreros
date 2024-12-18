d5062e665ca052c6fe86e3a332e064008bef751f
aj-cabreros <174784983+aj-cabreros@users.noreply.github.com>
2024-12-17 14:00:41 -1000

    [+] Complete Exercises 01_helloWorld and 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str, nTimes) {
+  if (nTimes < 0) return 'ERROR'
 
-};
-
+  let newStr = ''
+  while(nTimes > 0){
+    newStr += str
+    nTimes--
+  }
+  return newStr
+}
 // Do not edit below this line
 module.exports = repeatString;
