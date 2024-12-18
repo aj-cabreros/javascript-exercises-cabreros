4832688c8a234751cb7d9d6c1b9e43d259b8e3b4
aj-cabreros <174784983+aj-cabreros@users.noreply.github.com>
2024-12-18 12:24:56 -1000

    [+] Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function (arrayOfNumbers) {
+  sumOfNumbers = arrayOfNumbers.reduce((total, num) => total + num, 0)
+  return sumOfNumbers
 };
 
-const multiply = function() {
-
+const multiply = function (arrayOfNumbers) {
+  productOfNumbers = arrayOfNumbers.reduce((total, num) => total * num, 1)
+  return productOfNumbers
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  let result = base
+  for (let n = 1; n < exponent; n++)
+    result *= base
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) return 1
+  else {
+    let result = num
+    for (let n = num - 1; n > 1; n--)
+      result *= n
+    return result
+  }
+
 };
 
 // Do not edit below this line
