f657bf246a127d731d383b288c7bff84cd695af3
aj-cabreros <174784983+aj-cabreros@users.noreply.github.com>
2024-12-18 09:58:47 -1000

    [+] Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
+function isValid(num) {
+  if (typeof num === 'number'
+    && isFinite(num)
+    && num > 0
+    && Number.isInteger(num)) {
+    return true
+  } else {
+    return false
+  }
+}
 
-};
+const sumAll = function (num1, num2) {
+  if (isValid(num1) && isValid(num2)) {
+    let sum = 0
+    let initialValue = 0
+    let uptoValue = 0
+    if (num1 < num2) {
+      initialValue = num1
+      uptoValue = num2
+    } else {
+      initialValue = num2
+      uptoValue = num1
+    }
+    for (let n = initialValue; n <= uptoValue; n++) {
+      sum += n
+    }
 
+    return sum
+  } else {
+    return 'ERROR'
+  }
+
+
+}
 // Do not edit below this line
 module.exports = sumAll;
