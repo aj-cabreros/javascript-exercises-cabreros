751ae0da78428ad63c886a4b29d7d4c3062359d5
aj-cabreros <174784983+aj-cabreros@users.noreply.github.com>
2024-12-18 10:59:33 -1000

    [+] Complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const divisableBy100 = (year % 100 === 0)
+  const divisableBy400 = (year % 400 === 0)
+  const divisableBy4   = (year %   4 === 0)
 
+  if (divisableBy100)
+    if (divisableBy400) return true
+    else return false
+
+  if (divisableBy4) return true
+  else return false
 };
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
