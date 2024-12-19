61ace93f8570a7e892a570554ca4e65992b7087c
aj-cabreros <174784983+aj-cabreros@users.noreply.github.com>
2024-12-18 18:22:29 -1000

    [+] Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const getAge = function (person) {
+  if (person.hasOwnProperty('yearOfDeath'))
+    age = person.yearOfDeath - person.yearOfBirth
+  else
+    age = new Date().getFullYear() - person.yearOfBirth
+  return age
+}
 
+const findTheOldest = function (people) {
+  /* fuction takes array and returns object */
+  let eldestPerson = people[0]
+  let eldestAge = getAge(eldestPerson)
+  let personAge
+
+  people.forEach(function (person) {
+    personAge = getAge(person)
+    if (eldestAge < personAge)
+      eldestPerson = person
+    eldestAge = personAge
+  })
+  return eldestPerson
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
